46c7bcba76f3fd8e6bc78516f0cfc803e01faa04
Yehudit-Brickner <nech.diti@yahoo.com>
2022-05-30 23:24:26 +0300

    last

--- ex4_main.py
+++ ex4_main.py
@@ -21,30 +21,27 @@ def main():
     print("ID:", 328601018)
 
     # Read images
-    i =2
+    i =0
     L = cv2.imread(os.path.join('input', 'pair%d-L.png' % i), 0) / 255.0
     R = cv2.imread(os.path.join('input', 'pair%d-R.png' % i), 0) / 255.0
 
-    # L=cv2.imread('input/pair1-L.png',0)/255.0
-    # R=cv2.imread('input/pair1-R.png',0)/255.0
-    # # Display depth SSD
-    # displayDepthImage(L, R, (10,80), method=disparitySSD)
+    # Display depth SSD
+    displayDepthImage(L, R, (0,4), method=disparitySSD)
 
     # Display depth NC
-    # displayDepthImage(L, R, (10,80), method=disparityNC)
+    displayDepthImage(L, R, (0,4), method=disparityNC)
 
-    # src = np.array([[279, 552],
-    #                 [372, 559],
-    #                 [362, 472],
-    #                 [277, 469]])
-    # dst = np.array([[24, 566],
-    #                 [114, 552],
-    #                 [106, 474],
-    #                 [19, 481]])
-    # h, error = computeHomography(src, dst)
-    # #
-    # print(h, error)
-    # #
+    src = np.array([[279, 552],
+                    [372, 559],
+                    [362, 472],
+                    [277, 469]])
+    dst = np.array([[24, 566],
+                    [114, 552],
+                    [106, 474],
+                    [19, 481]])
+    h, error = computeHomography(src, dst)
+    print(h, error)
+
     dst = cv2.imread(os.path.join('input', 'billBoard.jpg'))[:, :, [2, 1, 0]] / 255.0
     src = cv2.imread(os.path.join('input', 'car.jpg'))[:, :, [2, 1, 0]] / 255.0
 
