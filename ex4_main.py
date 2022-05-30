f8797f636e8e91ae853d2a50a970b3fb96617e9e
Yehudit-Brickner <nech.diti@yahoo.com>
2022-05-30 17:37:42 +0300

    2a

--- ex4_main.py
+++ ex4_main.py
@@ -7,8 +7,6 @@ import time
 def displayDepthImage(l_img, r_img, disparity_range=(0, 5), method=disparitySSD):
     p_size = 5
     st = time.time()
-
-
     d_ssd = method(l_img, r_img, disparity_range, p_size)
     print("Time: {:.3f} sec".format(time.time() - st))
     plt.matshow(d_ssd)
@@ -22,30 +20,30 @@ def main():
     print("ID:", 328601018)
 
     # Read images
-    i = 0
+    i =1
     L = cv2.imread(os.path.join('input', 'pair%d-L.png' % i), 0) / 255.0
     R = cv2.imread(os.path.join('input', 'pair%d-R.png' % i), 0) / 255.0
 
     # L=cv2.imread('input/pair1-L.png',0)/255.0
     # R=cv2.imread('input/pair1-R.png',0)/255.0
     # # Display depth SSD
-    displayDepthImage(L, R, (0, 4), method=disparitySSD)
+    # displayDepthImage(L, R, (10,80), method=disparitySSD)
 
     # Display depth NC
-    # displayDepthImage(L, R, (0, 4), method=disparityNC)
+    # displayDepthImage(L, R, (0,4), method=disparityNC)
 
-    # src = np.array([[279, 552],
-    #                 [372, 559],
-    #                 [362, 472],
-    #                 [277, 469]])
-    # dst = np.array([[24, 566],
-    #                 [114, 552],
-    #                 [106, 474],
-    #                 [19, 481]])
-    # h, error = computeHomography(src, dst)
+    src = np.array([[279, 552],
+                    [372, 559],
+                    [362, 472],
+                    [277, 469]])
+    dst = np.array([[24, 566],
+                    [114, 552],
+                    [106, 474],
+                    [19, 481]])
+    h, error = computeHomography(src, dst)
     #
-    # print(h, error)
-    #
+    print(h, error)
+    # #
     # dst = cv2.imread(os.path.join('input', 'billBoard.jpg'))[:, :, [2, 1, 0]] / 255.0
     # src = cv2.imread(os.path.join('input', 'car.jpg'))[:, :, [2, 1, 0]] / 255.0
     #
--- ex4_utils.py
+++ ex4_utils.py
@@ -13,53 +13,57 @@ def disparitySSD(img_l: np.ndarray, img_r: np.ndarray, disp_range: (int, int), k
     return: Disparity map, disp_map.shape = Left.shape
     """
 
+    # diff = img_l - img_r
+    # plt.imshow(diff)
+    # plt.show()
+
     # create a blank image
     new = np.zeros((img_l.shape[0], img_l.shape[1]))
-    # get a gaussian kernel 5x1
-    # k = cv2.getGaussianKernel(5, -1)
-    # make it a square 5x5
-    # ker = k.dot(k.T)
-    # img_l=cv2.filter2D(img_l, -1, ker, borderType=cv2.BORDER_REPLICATE)
-    # img_r=cv2.filter2D(img_r, -1, ker, borderType=cv2.BORDER_REFLECT_101)
-    # go over all the value in the left image
-    # for r in range(img_l.shape[0]):
-    #     for c in range(img_l.shape[1]):
-    #         # go over the "window" around r,c
-    #         # min = float("inf")
-    #         # best_d = -1
-    #         sum = 0
-    #         for i in range(k_size * 2 + 1):
-    #             for j in range(k_size * 2 + 1):
-    #                 # check if the values are in the other image
-    #                 if 0 <= (r + i - (k_size * 2 + 1) // 2) < img_r.shape[0] and 0 <= (c + j - (k_size * 2 + 1) // 2) < img_r.shape[1]:
-    #                     # add to sum this number from the equation
-    #                     sum += (img_l[r][c] - img_r[r + i - (k_size * 2 + 1) // 2][c + j - (k_size * 2 + 1) // 2]) ** 2
-    #         # put the sum into the new image
-    #         new[r][c] = sum
-    # offset_adjust = 255 / disp_range[1]
-    # for r in range(k_size, img_l.shape[0]-k_size):
-    #     for c in range(k_size, img_l.shape[1]-k_size):
-    kernel=k_size*2+1
+    #
+    img_l_pad=cv2.copyMakeBorder(img_l,k_size+disp_range[1],k_size+disp_range[1],k_size+disp_range[1],k_size+disp_range[1],cv2.BORDER_REFLECT_101)
+    img_r_pad=cv2.copyMakeBorder(img_r,k_size+disp_range[1],k_size+disp_range[1],k_size+disp_range[1],k_size+disp_range[1],cv2.BORDER_REFLECT_101)
+
+   # # go over the image
+   #  for r in range(img_l.shape[0]):
+   #      for c in range(img_l.shape[1]):
+   #          best_offset = -1
+   #          prev_ssd = float("inf")
+   #          # go over the offset
+   #          for offset in range(disp_range[0], disp_range[1]):
+   #              ssd = 0
+   #              if r-k_size>=0 and r+k_size+1<img_l.shape[0] and c-k_size>=0 and c+k_size+1<img_l.shape[1]: # check that we are in range in the left image
+   #                  if r-k_size>=0 and r+k_size+1<img_r.shape[0] and c-k_size-offset>=0 and c+k_size-offset+1<img_r.shape[1]: # check that we are in range in the right image
+   #                      ssd = np.sum((img_l[r-k_size:r+k_size+1,c-k_size:c+k_size+1]-img_r[r-k_size:r+k_size+1,c-k_size-offset:c+k_size-offset+1])**2)
+   #              # check if smaller then prev
+   #              if ssd < prev_ssd:
+   #                  prev_ssd = ssd
+   #                  best_offset = offset
+   #          new[r][c] = best_offset #put in the value in the new image
+    # go over the image
     for r in range(img_l.shape[0]):
         for c in range(img_l.shape[1]):
-            best_offset = -1
+            r1=r+k_size+disp_range[1]
+            c1=c+k_size+disp_range[1]
+            best_offset =0
             prev_ssd = float("inf")
+            # go over the offset
             for offset in range(disp_range[0], disp_range[1]):
                 ssd = 0
-                # run over kersize
-                # for v in range(kernel):
-                #     for u in range(kernel):
-                        # if 0 <= r + v - offset < img_r.shape[0] and 0 <= c + u - offset < img_r.shape[1]:
-                            # ssd += ((img_l[r, c]) - (img_r[r + v - offset, c + u - offset])) ** 2
-                if r-k_size>=0 and r+k_size<img_l.shape[0] and c-k_size>=0 and c+k_size<img_l.shape[1]:
-                    if r-k_size>=0 and r+k_size<img_r.shape[0] and c-k_size-offset>=0 and c+k_size-offset<img_r.shape[1]:
-                        ssd += np.sum((img_l[r-k_size:r+k_size+1,c-k_size:c+k_size+1]-img_r[r-k_size:r+k_size+1,c-k_size-offset:c+k_size-offset+1])**2)
+                if r1 - k_size >= 0 and r1 + k_size + 1 < img_l_pad.shape[0] and c1 - k_size >= 0 and c1 + k_size + 1 < img_l_pad.shape[1]:  # check that we are in range in the left image
+                    if r1 - k_size >= 0 and r1 + k_size + 1 < img_r_pad.shape[0] and c1 - k_size - offset >= 0 and c1 + k_size - offset + 1 < img_r_pad.shape[1]:  # check that we are in range in the right image
+                        ssd = np.sum((img_l_pad[r1 - k_size:r1 + k_size + 1, c1 - k_size:c1 + k_size + 1] - img_r_pad[r1 - k_size:r1 + k_size + 1,c1 - k_size - offset:c1 + k_size + 1 - offset]) ** 2)
+                # check if smaller then prev
                 if ssd < prev_ssd:
                     prev_ssd = ssd
                     best_offset = offset
+            # put in the value in the new image
             new[r][c] = best_offset
-    # print(new)
-
+            # if (r+1 < img_l.shape[0]):
+            #     new[r+1][c] = best_offset
+            # if c+1<=img_l.shape[1]:
+            #     new[r ][c+1] = best_offset
+            # if r+1 <img_l.shape[0] and c+1 <img_l.shape[1]:
+            #     new[r+1][c + 1] = best_offset322
     return new
 
 
@@ -72,7 +76,9 @@ def disparityNC(img_l: np.ndarray, img_r: np.ndarray, disp_range: (int,int), k_s
 
     return: Disparity map, disp_map.shape = Left.shape
     """
-
+    # diff= img_l-img_r
+    # plt.imshow(diff)
+    # plt.show()
     # create a blank image
     new = np.zeros((img_l.shape[0], img_l.shape[1]))
     # go over all the value in the left image
@@ -104,25 +110,35 @@ def disparityNC(img_l: np.ndarray, img_r: np.ndarray, disp_range: (int,int), k_s
     #             new[r][c] = top / np.sqrt(bottom1 * bottom2)
     #         else:
     #             new[r][c] = top
-    kernel=k_size*2+1
+    # kernel=k_size*2+1
     for r in range(img_l.shape[0]):
+        print(r)
         for c in range(img_l.shape[1]):
             best_offset = 0
             prev_ssd = float("inf")
             for offset in range(disp_range[0], disp_range[1]):
                 ssd = 0
-                top = 0
-                bottom1=0
-                bottom2=0
+                # top = 0
+                # bottom1=0
+                # bottom2=0
+                if r - k_size >= 0 and r + k_size < img_l.shape[0] and c - k_size >= 0 and c + k_size < img_l.shape[1]:
+                    if r - k_size >= 0 and r + k_size < img_r.shape[0] and c - k_size - offset >= 0 and c + k_size - offset < img_r.shape[1]:
+                        top = np.sum((img_l[r - k_size:r + k_size + 1, c - k_size:c + k_size + 1] - img_r[r - k_size:r + k_size + 1,c - k_size - offset:c + k_size - offset + 1]) ** 2)
+                        bottom1 = np.sum((img_l[r - k_size:r + k_size + 1, c - k_size:c + k_size + 1] - img_l[r - k_size:r + k_size + 1,c - k_size - offset:c + k_size - offset + 1]) ** 2)
+                        bottom2 = np.sum((img_r[r - k_size:r + k_size + 1, c - k_size:c + k_size + 1] - img_r[r - k_size:r + k_size + 1,c - k_size - offset:c + k_size - offset + 1]) ** 2)
+                        if bottom1*bottom2!=0:
+                            ssd=top/bottom1*bottom2
+                        else:
+                            ssd=top
                 # run over kersize
-                for v in range(kernel):
-                    for u in range(kernel):
-                        if 0 <= r + v - offset < img_r.shape[0] and 0 <= c + u - offset < img_r.shape[1]:
-                            top += ((img_l[r, c]) * (img_r[r + v - offset, c + u - offset])) ** 2
-                            bottom1 += ((img_r[r, c]) * (img_r[r + v - offset, c + u - offset])) ** 2
-                            bottom2 += ((img_l[u, v]) * (img_l[offset, offset])) ** 2
-                            if bottom1*bottom2!=0:
-                                ssd=top/np.sqrt(bottom1*bottom2)
+                # for v in range(kernel):
+                #     for u in range(kernel):
+                #         if 0 <= r + v - offset < img_r.shape[0] and 0 <= c + u - offset < img_r.shape[1]:
+                #             top += ((img_l[r, c]) * (img_r[r + v - offset, c + u - offset])) ** 2
+                #             bottom1 += ((img_r[r, c]) * (img_r[r + v - offset, c + u - offset])) ** 2
+                #             bottom2 += ((img_l[u, v]) * (img_l[offset, offset])) ** 2
+                #             if bottom1*bottom2!=0:
+                #                 ssd=top/np.sqrt(bottom1*bottom2)
                             # else:
                             #     ssd=top
 
@@ -150,40 +166,40 @@ def computeHomography(src_pnt: np.ndarray, dst_pnt: np.ndarray) -> (np.ndarray, 
     A = np.zeros((src_pnt.shape[0] * 2, 9))
     # A=np.array([])
     for x in range(src_pnt.shape[0]):
-        A[2 * x] = np.array(
-            [src_pnt[x][0], src_pnt[x][1], 1, 0, 0, 0, -dst_pnt[x][0] * src_pnt[x][0], -dst_pnt[x][0] * src_pnt[x][1],
-             -dst_pnt[x][0]])
-        A[2 * x + 1] = np.array(
-            [0, 0, 0, src_pnt[x][0], src_pnt[x][1], 1, -dst_pnt[x][1] * src_pnt[x][0], -dst_pnt[x][1] * src_pnt[x][1],
-             -dst_pnt[x][1]])
+        A[2 * x] = np.array( [src_pnt[x][0], src_pnt[x][1], 1, 0, 0, 0, -dst_pnt[x][0] * src_pnt[x][0], -dst_pnt[x][0] * src_pnt[x][1],-dst_pnt[x][0]])
+        A[2 * x + 1] = np.array([0, 0, 0, src_pnt[x][0], src_pnt[x][1], 1, -dst_pnt[x][1] * src_pnt[x][0], -dst_pnt[x][1] * src_pnt[x][1],-dst_pnt[x][1]])
 
     # get the svd of A and use it to get the h vector
     u, s, vh = np.linalg.svd(A)
     # reshape and divide by the number in the third row third col
     hom = vh[-1].reshape(3, 3)
     hom = hom / hom[2][2]
-    # print(hom)
-    # hom2, e2 = cv2.findHomography(src_pnt,dst_pnt)
-    # print(hom2)
+
 
     # get the error
-    diff = src_pnt[0:3, :] - dst_pnt[0:3, :]
-    ddd = hom.dot(diff)
-    error = np.sqrt((np.sum(ddd) ** 2))
+    # create a one array to add to connect with src and dst points
+    one=np.ones(src_pnt.shape[0]).reshape(src_pnt.shape[0],1)
+    # add the one and switch direction
+    src_points = np.concatenate((src_pnt,one), axis=1).T
+    dst_points = np.concatenate((dst_pnt,one), axis=1).T
+    # warp the points
+    diff=hom.dot(src_points)
+    # normalize the points
+    diff=diff/diff[2,:]
+    # subtract the dest points
+    diff=diff-dst_points
+    # get the error
+    error=np.sqrt(np.sum(diff**2))
 
     return hom, error
 
 
 def getEquation(p1, p2):
-    top = p1[1] - p2[1]  # y1-y2
-    bottom = p1[0] - p2[0]  # x1-x2
+    top = int(p1[1]) - int(p2[1])  # y1-y2
+    bottom = int(p1[0]) - int(p2[0])  # x1-x2
     if bottom != 0:
         slope = top / bottom
-        # y-y1=slope(x-x1)
-        # y= slopex -slopex1+y1
-        # y=1
-        # m=slope
-        b = -slope * p1[0] + p1[1]
+        b = -slope * int(p1[0]) + int(p1[1])
         print("y=", slope, "x ", b)
         return slope, b
     else:
@@ -294,9 +310,8 @@ def warpImag(src_img: np.ndarray, dst_img: np.ndarray) -> None:
     TL_BL_slope, TL_BL_b = getEquation((dst_p[tl][0], dst_p[tl][1]), (dst_p[bl][0], dst_p[bl][1]))
     TR_BR_slope, TR_BR_b = getEquation((dst_p[br][0], dst_p[br][1]), (dst_p[tr][0], dst_p[tr][1]))
 
-
-
-    for x in range(dst_img.shape[0]): # go over rows of image
+    # go over image
+    for x in range(dst_img.shape[0]):
         for y in range(dst_img.shape[1]):
             if minX <= y <= maxX and minY <= x <= maxY:  # (opposite because of how plt plots)
                 if TL_TR_slope * y + TL_TR_b <= x <= BL_BR_slope * y + BL_BR_b:  # check if the x is in the okay range (opposite because of how plt plots- should be checking the col)
@@ -321,28 +336,17 @@ def warpImag(src_img: np.ndarray, dst_img: np.ndarray) -> None:
                             mask1[x][y][1] = 1
                             mask1[x][y][2] = 1
 
-    # plt.imshow(mask1)
-    # plt.show()
+    plt.imshow(mask1)
+    plt.show()
 
-    # # make the mask of the image
-    # mask = np.zeros((dst_img.shape[0], dst_img.shape[1], 3))
-    # for j in range(dst_img.shape[0]):
-    #     for i in range(dst_img.shape[1]):
-    #         if i > dst_p[tl][0] and i > dst_p[bl][0] and i < dst_p[br][0] and i < dst_p[tr][0] \
-    #                 and j > dst_p[tl][1] and j > dst_p[tr][1] and j < dst_p[br][1] and j < dst_p[bl][1]:
-    #             mask[j][i][0] = 1
-    #             mask[j][i][1] = 1
-    #             mask[j][i][2] = 1
-    # plt.imshow(mask)
-    # plt.show()
 
     # get the homgraphy of teh images
     hom, e = computeHomography(src_p, dst_p)
     theta = 1.5708
-    turn = np.array([[np.cos(theta), -np.sin(theta), (dst_img.shape[0] * 3 // 4)],
-                     [np.sin(theta), np.cos(theta), 0],
-                     [0, 0, 1]], dtype=np.float)
-    hom = hom @ turn
+    # turn = np.array([[np.cos(theta), -np.sin(theta), (dst_img.shape[0] * 3 // 4)],
+    #                  [np.sin(theta), np.cos(theta), 0],
+    #                  [0, 0, 1]], dtype=np.float)
+    # hom = hom @ turn
     # print(hom)
     # hom1 , e1= cv2.findHomography(src_p.astype(float), dst_p.astype(float))
     # print(hom1)
@@ -350,10 +354,10 @@ def warpImag(src_img: np.ndarray, dst_img: np.ndarray) -> None:
 
     src_out = cv2.warpPerspective(src_img, hom, (dst_img.shape[1], dst_img.shape[0]))
 
-    # plt.imshow(src_out)
-    # plt.show()
-    # plt.imshow(mask1 -src_out)
-    # plt.show()
+    plt.imshow(src_out)
+    plt.show()
+    plt.imshow(mask1 -src_out)
+    plt.show()
     # connect the images
     out = dst_img * (1 - mask1) + src_out * (mask1)
     plt.imshow(out)
